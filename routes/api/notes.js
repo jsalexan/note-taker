780895d4c4722fdc3d011ecb4ccf8ded56673e6f
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-10-31 16:20:21 -0700

    Added more code to the server.js and notes.js. Trying to figure out the UUID part and the fs. Need to whiteboard some of this because there's a lot going on

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,1 +1,34 @@
 // API ROUTES
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+const db = require("../../db/db.json");
+
+notes.get('/', (req, res) => {
+    readFromFile('../../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+ 
+    const { id, title, text } =req.body;
+
+    if (req.body) {
+        const newNote = {
+            id,
+            title,
+            text
+        };
+
+        readAndAppend(newNote, '../../db/db.json')
+        res.json('Your note was added with MUCH success!');
+    } else {
+        res.error('Oh no! There was an error adding your beautiful note!');
+    }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
-
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
