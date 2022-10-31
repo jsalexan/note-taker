95852581598311211276d1922cdb1405b1d3ebfe
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-10-31 15:15:39 -0700

    Got the notes page linked and rendering with the button push. Added 404 page and it's working

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const path = require('path');
 const PORT = process.env.PORT || 3005;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
 app.use(express.static("public"));
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
 
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
 
 
 
